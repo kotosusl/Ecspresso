697b3a5e561b8d51c7ac150fb783e85ab474e492
kotosusl <108192472+kotosusl@users.noreply.github.com>
2023-02-21 00:09:08 +0700

    add_form

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.QtSql import QSqlDatabase, QSqlQuery, QSqlTableModel
 from PyQt5 import uic
 import sys
+import sqlite3
 
 if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
     QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
@@ -15,7 +16,7 @@ class Espresso(QMainWindow):
     def __init__(self):
         super(Espresso, self).__init__()
         uic.loadUi('main.ui', self)
-        self.setFixedSize(561, 447)
+        self.setFixedSize(561, 490)
         self.db = QSqlDatabase.addDatabase('QSQLITE')
         self.db.setDatabaseName('coffee.sqlite')
         self.project_model = QSqlTableModel(self)
@@ -24,15 +25,125 @@ class Espresso(QMainWindow):
         self.tableView.setEditTriggers(QtWidgets.QAbstractItemView.EditTrigger.NoEditTriggers)
         self.tableView.setSelectionMode(QtWidgets.QAbstractItemView.SelectionMode.SingleSelection)
         self.tableView.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectionBehavior.SelectRows)
+        self.update_table()
+        self.add.clicked.connect(self.adding)
+        self.refactor.clicked.connect(self.reset)
+        self.update_button.clicked.connect(self.update_table)
+
+    def adding(self):
+        global IN_FORM
+        if not IN_FORM:
+            IN_FORM = True
+            self.form = AddForm()
+            self.form.show()
+
+    def reset(self):
+        global IN_FORM
+        if not IN_FORM:
+            if str(self.tableView.currentIndex().siblingAtColumn(0).data()):
+                IN_FORM = True
+                self.form = ResetForm(self.tableView.currentIndex().siblingAtColumn(0).data())
+                self.form.show()
+
+    def update_table(self):
         sql = QSqlQuery('''select coffee.ID, sorts.sort as "Сорт", roasting.roasting as "Степень обжарки",
-        types.type as "Тип", coffee.volume as "Объём", coffee.cost as "Стоимость", coffee.description as "Описание"
-        from coffee
-        join sorts on sorts.id = coffee.sorts
-        join roasting on roasting.ID = coffee.roasting
-        join types on types.ID = coffee.type''', self.db)
+                types.type as "Тип", coffee.volume as "Объём", coffee.cost as "Стоимость", 
+                coffee.description as "Описание"
+                from coffee
+                join sorts on sorts.id = coffee.sorts
+                join roasting on roasting.ID = coffee.roasting
+                join types on types.ID = coffee.type''', self.db)
         self.project_model.setQuery(sql)
 
 
+class ResetForm(Espresso):
+    def __init__(self, coffee_id):
+        super(ResetForm, self).__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.setFixedSize(390, 321)
+        con = sqlite3.connect('coffee.sqlite')
+        self.cur = con.cursor()
+        self.coffee_id = coffee_id
+        self.sort.addItems([str(p[0]) for p in self.cur.execute('''select sort from sorts''').fetchall()])
+        self.roasting.addItems([str(p[0]) for p in self.cur.execute('''select roasting from roasting''').fetchall()])
+        self.type.addItems([str(p[0]) for p in self.cur.execute('''select type from types''').fetchall()])
+        self.sort.setCurrentText(str(self.cur.execute(f'''select sorts.sort from sorts where sorts.id = 
+        (select coffee.sorts from coffee where coffee.id = {coffee_id})''').fetchone()[0]))
+        self.roasting.setCurrentText(str(self.cur.execute(f'''select roasting from roasting where roasting.id = 
+        (select coffee.roasting from coffee where coffee.id = {coffee_id})''').fetchone()[0]))
+        self.type.setCurrentText(str(self.cur.execute(f'''select type from types where types.id = 
+        (select coffee.type from coffee where coffee.id = {coffee_id})''').fetchone()[0]))
+        self.cost.setValue(self.cur.execute(f'''select coffee.cost from coffee 
+                where coffee.id = {coffee_id}''').fetchone()[0])
+        self.volume.setValue(self.cur.execute(f'''select coffee.volume from coffee 
+                where coffee.id = {coffee_id}''').fetchone()[0])
+        self.description.setText(self.cur.execute(f'''select description from coffee 
+                where coffee.id = {coffee_id}''').fetchone()[0])
+        self.add_button.clicked.connect(self.new_row)
+
+    def new_row(self):
+        if (self.sort.currentText(), self.roasting.currentText(), self.type.currentText(), self.volume.value(),
+            self.cost.value()) in self.cur.execute('''select sorts.sort, roasting.roasting, types.type,
+             coffee.volume, coffee.cost from coffee
+                join sorts on sorts.id = coffee.sorts
+                join roasting on roasting.ID = coffee.roasting
+                join types on types.ID = coffee.type''').fetchall():
+            self.statusbar.showMessage('Запись уже существует')
+        else:
+            self.statusbar.showMessage('')
+            sql = f'''update coffee
+            set sorts = (select id from sorts where sort = '{self.sort.currentText()}'), 
+            roasting = (select id from roasting where roasting = '{self.roasting.currentText()}'),
+            type = (select id from types where type = '{self.type.currentText()}'),
+            volume = {self.volume.value()}, cost = {self.cost.value()},
+            description = '{self.description.toPlainText()}'
+            where coffee.id = {self.coffee_id}'''
+            self.sqlquery = QSqlQuery()
+            self.sqlquery.exec(sql)
+            self.close()
+
+    def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
+        global IN_FORM
+        IN_FORM = False
+
+
+class AddForm(Espresso):
+    def __init__(self):
+        super(AddForm, self).__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.setFixedSize(390, 321)
+        con = sqlite3.connect('coffee.sqlite')
+        self.cur = con.cursor()
+        self.sort.addItems([str(p[0]) for p in self.cur.execute('''select sort from sorts''').fetchall()])
+        self.roasting.addItems([str(p[0]) for p in self.cur.execute('''select roasting from roasting''').fetchall()])
+        self.type.addItems([str(p[0]) for p in self.cur.execute('''select type from types''').fetchall()])
+        self.add_button.clicked.connect(self.new_row)
+
+    def new_row(self):
+        if (self.sort.currentText(), self.roasting.currentText(), self.type.currentText(), self.volume.value(),
+            self.cost.value()) in self.cur.execute('''select sorts.sort, roasting.roasting, types.type,
+             coffee.volume, coffee.cost from coffee
+                join sorts on sorts.id = coffee.sorts
+                join roasting on roasting.ID = coffee.roasting
+                join types on types.ID = coffee.type''').fetchall():
+            self.statusbar.showMessage('Запись уже существует')
+        else:
+            self.statusbar.showMessage('')
+            sql = f'''INSERT INTO coffee(sorts, roasting, type, volume, cost, description) 
+            VALUES((select id from sorts where sort = '{self.sort.currentText()}'), 
+            (select id from roasting where roasting = '{self.roasting.currentText()}'),
+            (select id from types where type = '{self.type.currentText()}'),
+            {self.volume.value()}, {self.cost.value()}, '{self.description.toPlainText()}')'''
+            self.sqlquery = QSqlQuery()
+            self.sqlquery.exec(sql)
+            self.close()
+
+    def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
+        global IN_FORM
+        IN_FORM = False
+
+
+IN_FORM = False
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Espresso()
